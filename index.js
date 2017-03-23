6d512a2942b8066887e8049c82ec4bc91583e199
Sebastian Edwards <me@sebastianedwards.co.nz>
2017-03-23 13:25:35 +1300

    Move non-noscript tag into head.

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ module.exports = {
   contentFor: function(type, config) {
     var config = config['ember-cli-gtm']
     var appId = config && config.appId
-    if (type === 'body' && appId) {
-      return "<noscript><iframe src='//www.googletagmanager.com/ns.html?id=" + appId + "' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','" + appId + "');</script>";
+    if (appId) {
+      if (type === 'body') {
+        return "<noscript><iframe src='//www.googletagmanager.com/ns.html?id=" + appId + "' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>";
+      } else if (type === 'head') {
+        return "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','" + appId + "');</script>";
+      }
     }
   }
 };
